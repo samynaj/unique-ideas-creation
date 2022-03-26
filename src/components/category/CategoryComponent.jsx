4c8eb873691b5356c320025260403f47a52a0f00
samynaj <samuelnnj@gmail.com>
2022-03-26 23:54:12 +0100

    first commit

---
+++ src/components/category/CategoryComponent.jsx
@@ -0,0 +1,90 @@
+import './category.css'
+
+const CategoryComponent = () => {
+  return (
+    <div className='category'>
+        <div className="category-top">
+            <hr className='category-hr' />
+            <h2 className="category-title">Collections</h2>
+            <hr className='category-hr' />
+        </div>
+        
+        <div className="categoryContainer">
+            <div className="category-item">
+                <div className="category-item-img"></div>
+                <div className="category-item-footer">
+                    <span className="item-name"></span>
+                    <span className="item-price"></span>
+                </div>
+                <div className="item-button">Add to Cart</div>
+            </div>
+            <div className="category-item">
+                <div className="category-item-img"></div>
+                <div className="category-item-footer">
+                    <span className="item-name"></span>
+                    <span className="item-price"></span>
+                </div>
+                <div className="item-button">Add to Cart</div>
+            </div>
+            <div className="category-item">
+                <div className="category-item-img"></div>
+                <div className="category-item-footer">
+                    <span className="item-name"></span>
+                    <span className="item-price"></span>
+                </div>
+                <div className="item-button">Add to Cart</div>
+            </div>
+            <div className="category-item">
+                <div className="category-item-img"></div>
+                <div className="category-item-footer">
+                    <span className="item-name"></span>
+                    <span className="item-price"></span>
+                </div>
+                <div className="item-button">Add to Cart</div>
+            </div>
+            <div className="category-item">
+                <div className="category-item-img"></div>
+                <div className="category-item-footer">
+                    <span className="item-name"></span>
+                    <span className="item-price"></span>
+                </div>
+                <div className="item-button">Add to Cart</div>
+            </div>
+            <div className="category-item">
+                <div className="category-item-img"></div>
+                <div className="category-item-footer">
+                    <span className="item-name"></span>
+                    <span className="item-price"></span>
+                </div>
+                <div className="item-button">Add to Cart</div>
+            </div>
+            <div className="category-item">
+                <div className="category-item-img"></div>
+                <div className="category-item-footer">
+                    <span className="item-name"></span>
+                    <span className="item-price"></span>
+                </div>
+                <div className="item-button">Add to Cart</div>
+            </div>
+            <div className="category-item">
+                <div className="category-item-img"></div>
+                <div className="category-item-footer">
+                    <span className="item-name"></span>
+                    <span className="item-price"></span>
+                </div>
+                <div className="item-button">Add to Cart</div>
+            </div>
+            <div className="category-item">
+                <div className="category-item-img"></div>
+                <div className="category-item-footer">
+                    <span className="item-name"></span>
+                    <span className="item-price"></span>
+                </div>
+                <div className="item-button">Add to Cart</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CategoryComponent
